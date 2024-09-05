1000df59c5ed68f4055338f6f027eca58ad2e3cf
Sergey <sigoshin2@gmail.com>
2024-09-05 13:01:02 +0300

    UserValidator created

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 # from config import settings
-from src.database.orm import AsyncCore
+from src.database.orm import AsyncCore, User, Task
 import asyncio
 
-from src.database.validation import Validator
+from src.database.validation import UserValidator
+from typing import List
 
 
 async def main():
     # await AsyncCore.insert_user()
-    # await AsyncCore.select_users()
-    await AsyncCore.update_user()
+    # users: List["User"] = await AsyncCore.select_users()
+    # for user in users:
+    #     print(user.id)
+    tasks: List["Task"] = await AsyncCore.select_tasks()
+    for task in tasks:
+        print(task.id)
+
+    # await AsyncCore.update_user()
+    # await AsyncCore.insert_task("Second task", 1)
+    # res = await UserValidator.is_unique_email("jacck@gmail.com")
+    # res = await UserValidator.is_unique_username("Travis")
+    # print(f"{res=}")
+
 
 if __name__ == '__main__':
-    # asyncio.run(main())
-    print(f"{Validator.is_valid_email("do.wdw2g@maio")}")
+    asyncio.run(main())
+    # print(f"{UserValidator.is_unique_email("jack@gmail.com")}")
--- src/database/orm.py
+++ src/database/orm.py
@@ -4,17 +4,22 @@ from src.database.model import (
     User,
     Task,
 )
-from sqlalchemy import select, update, delete
+from sqlalchemy import select, update
+from typing import List
 
 
 class AsyncCore:
     @staticmethod
-    async def insert_user():
+    async def insert_user(
+            username: str,
+            email: str,
+            password: str,
+    ):
         async with async_session() as session:
             user = User(
-                username='Takeoff',
-                email='Takeoff@gmail.com',
-                hashed_password='<PASSWORD>',
+                username=username,
+                email=email,
+                hashed_password=password,
             )
             try:
                 session.add(user)
@@ -23,35 +28,62 @@ class AsyncCore:
                 await session.rollback()
 
     @staticmethod
-    async def insert_task():
+    async def insert_task(
+            description: str,
+            user_id: int,
+    ):
         async with async_session() as session:
             task = Task(
-
+                description=description,
+                user_id=user_id,
             )
-            session.add(task)
-            session.commit()
+            try:
+                session.add(task)
+                await session.commit()
+            except IntegrityError:
+                await session.rollback()
 
     @staticmethod
-    async def select_tasks():
+    async def select_tasks(**kwargs) -> List["Task"]:
         async with async_session() as session:
             query = select(Task)
+
+            for key, value in kwargs.items():
+                if hasattr(User, key):
+                    query = query.where(getattr(Task, key) == value)
+
             result = await session.execute(query)
-            tasks = result.scalars().all()
-            print(tasks)
+            return result.scalars().all()
 
     @staticmethod
-    async def select_users():
+    async def select_users(**kwargs) -> List["User"]:
         async with async_session() as session:
             query = select(User)
+
+            for key, value in kwargs.items():
+                if hasattr(User, key):
+                    query = query.where(getattr(User, key) == value)
+
             result = await session.execute(query)
-            tasks = result.scalars().all()
-            for task in tasks:
-                print(task)
-            print(tasks)
+            return result.scalars().all()
 
     @staticmethod
-    async def update_user(user_id: int = 1, new_username: str = 'Travis'):
+    async def update_user_username(user_id: int, new_username: str):
         async with async_session() as session:
             user = await session.get(User, user_id)
             user.username = new_username
+            await session.commit()
+
+    @staticmethod
+    async def update_task_description(task_id: int, new_description: str):
+        async with async_session() as session:
+            task = await session.get(Task, task_id)
+            task.username = new_description
+            await session.commit()
+
+    @staticmethod
+    async def update_task_status(task_id: int, completed: bool):
+        async with async_session() as session:
+            task = await session.get(Task, task_id)
+            task.completed = completed
             await session.commit()
